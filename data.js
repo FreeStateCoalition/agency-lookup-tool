152340e67dd3f37db8528da30e342a0ac0a7f8e8
FreeStateCoalition <freestatecoalition@gmail.com>
2025-06-09 07:23:42 -0400

    Add files via upload

---
+++ data.js
@@ -0,0 +1,31 @@
+
+const data = [
+  {
+    name: "Environmental Protection Agency (EPA)",
+    dc_address: "1200 Pennsylvania Ave NW, Washington, DC 20460",
+    website: "https://www.epa.gov",
+    local_offices: [
+      { region: "Region 1 – Boston", address: "5 Post Office Square, Suite 100, Boston, MA 02109" },
+      { region: "Region 2 – New York", address: "290 Broadway, New York, NY 10007" },
+      { region: "Region 3 – Philadelphia", address: "1650 Arch Street, Philadelphia, PA 19103" }
+    ]
+  },
+  {
+    name: "Department of Justice (DOJ)",
+    dc_address: "950 Pennsylvania Avenue NW, Washington, DC 20530-0001",
+    website: "https://www.justice.gov",
+    local_offices: [
+      { region: "Eastern District of California", address: "501 I Street, Suite 10-100, Sacramento, CA 95814" },
+      { region: "Southern District of New York", address: "One St. Andrew's Plaza, New York, NY 10007" }
+    ]
+  },
+  {
+    name: "Social Security Administration (SSA)",
+    dc_address: "6401 Security Boulevard, Baltimore, MD 21235",
+    website: "https://www.ssa.gov",
+    local_offices: [
+      { region: "Chicago Regional Office", address: "230 S Dearborn Street, Room 2860, Chicago, IL 60604" },
+      { region: "San Francisco Regional Office", address: "90 7th Street, Suite 2-800, San Francisco, CA 94103" }
+    ]
+  }
+];
